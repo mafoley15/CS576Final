a13d9a9d9d32856dbf69777050ff9d214e1edea0
AdamKaluzny <adam.kaluzny222@gmail.com>
2024-12-15 22:22:19 -0500

    half implementation
    added game flow for first half of procedure + some minigame mechanics changes

--- Assets/MeasurePour.cs
+++ Assets/MeasurePour.cs
@@ -2,76 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MeasurePour : MonoBehaviour
 {
     private bool is_pouring;
-    private bool beaker_filled;
     private GameObject beaker_liquid;
     private float shifted_scale;
     private GameObject particles;
     public Text gradeUI;
     private string mixingGrade;
+
+    public float waterAmount;
+    public Text amount;
+
+    public float goal;
+
+    public Button doneButton;
+    public string scaleGrade;
+    public int labStep;
     // Start is called before the first frame update
     void Start()
     {
         is_pouring = false;
-        beaker_filled = false;
         beaker_liquid = GameObject.Find("Beakerwater");
         beaker_liquid.transform.localScale = new Vector3(0, 0, 0);
         shifted_scale = 0.0f;
+        waterAmount = 0.0f;
         particles = GameObject.Find("Particle System");
         particles.SetActive(false);
+        doneButton.onClick.AddListener(() =>  {
+            finishedMeasuring();
+         });
+        labStep = Lab.labStep;
+        if(labStep == 4){
+            goal = 500;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!is_pouring){
+        if(true){
             if(Input.GetKey(KeyCode.RightArrow)){
                 transform.Rotate(new Vector3(0, 0, -30 * Time.deltaTime), Space.Self);
             }
             if(Input.GetKey(KeyCode.LeftArrow)){
                 transform.Rotate(new Vector3(0, 0, 30 * Time.deltaTime), Space.Self);
             }
-            if(Input.GetKey(KeyCode.Space)){
-                is_pouring = true;
-            }
+
         }
-        if(is_pouring && !beaker_filled){
-            float z = GetComponent<Transform>().rotation.z;
-            //Debug.Log(z);
-            particles.SetActive(true);
-            if(z <= -0.65f || z >= 0.65f){
-                if((z <= -0.65f && z>= -0.77) || (z <= 0.65f && z>= 0.77)){
-                    // fill beaker
-                    shifted_scale += 0.01f;
-                    beaker_liquid.transform.localScale = new Vector3(8.0f, shifted_scale, 8.0f);
-                    is_pouring = false;
-                }
-            }
+
+        float z = GetComponent<Transform>().rotation.z;
+        if(z <= -0.7f){
+            is_pouring = true;
+        } else{
             is_pouring = false;
         }
+
+        if(is_pouring){
+            particles.SetActive(true);
+            if((z <= -0.65f && z>= -0.77)){
+                    // fill beaker
+                    shifted_scale += 0.01f;
+                    waterAmount = shifted_scale * 100;
+                    amount.text = "Amount in Beaker: " + Mathf.Min(800, waterAmount) + " ml";
+                    beaker_liquid.transform.localScale = new Vector3(8.0f, shifted_scale, 8.0f);
+            }
+        } else{
+           particles.SetActive(false);
+        }
+
         if(beaker_liquid.transform.localScale.y >= 8.0f){
-            particles.SetActive(false);
             beaker_liquid.transform.localScale = new Vector3(8.0f, 8.0f, 8.0f);
         }
-        if(beaker_liquid.transform.localScale.y == 8.0f){
-            beaker_filled = true;
-        }
-    }
 
+
+    }
+    private IEnumerator Wait()
+    {
+        // Wait for 1 second
+
+
+        yield return new WaitForSeconds(2f); // Wait for 3 seconds
+        SceneManager.LoadScene("Laboratory Scene");
+    }
     public void finishedMeasuring(){
-        // float timeToBeat = Time.timeSinceLevelLoad;
-        // if (timeToBeat < (10/3 * goal)){
-        //     mixingGrade = "A";
-        // } else if(timeToBeat < (15/3 * goal)){
-        //     mixingGrade = "B";
-        // } else if (timeToBeat < (20/3 * goal)){
-        //     mixingGrade = "C";
-        // } else {
-        //     mixingGrade = "F";
-        // }
-        // gradeUI.text = "Grade: " + mixingGrade;
+        
+        float difference = Mathf.Abs(waterAmount - goal);
+        if(difference < 10){
+            scaleGrade = "A";
+        } else if(difference < 25){
+            scaleGrade = "B";
+        } else if (difference < 75){
+            scaleGrade = "C";
+        } else {
+            scaleGrade = "F";
+        }
+        gradeUI.text = "Grade: " + scaleGrade;
+        Lab.labStep = labStep + 1;
+        StartCoroutine(Wait());
     }
 }
--- Assets/Scale.cs
+++ Assets/Scale.cs
@@ -2,25 +2,109 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class Scale : MonoBehaviour
 {
+    public GameObject game;
+    public GameObject grade;
     public Text current_mass;
+    public Text measurement;
     public float mass;
+    public bool unit_in_grams;
+    public Text gradeUI;
+    private string scaleGrade;
+    // goal needs to be set depending on procedure step
+    public float goal;
+
+    private int labStep;
+
+    public GameObject labObject;
     // Start is called before the first frame update
     void Start()
     {
-        current_mass.text = "Mass: 0.015 g";
+        game.SetActive(true);
+        grade.SetActive(false);
+        mass = 0.0f;
+        current_mass.text = "Mass: " + mass.ToString("#.000") + " g";
+        unit_in_grams = false;
+        measurement.text = "milligrams";
+        // labObject = GameObject.Find("LabObject");
+
+        // if (labObject == null)
+        // {
+        //     Debug.LogError("GameObject 'LabObject' not found! Check the name in the hierarchy.");
+        // } else{
+        //     Debug.Log("found lab object");
+        // }
+        // labStep = labObject.GetComponent<Lab>().labStep;
+        labStep = Lab.labStep;
+        if(labStep == 3){
+            goal = 0.002f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //maybe grade can be based on both correctness and timing?
+        current_mass.text = "Mass: " + mass.ToString("#.000") + " g";
+        if(unit_in_grams){
+            measurement.text = "grams";
+        }else{
+            measurement.text = "milligrams";
+        }
+        if(mass < 0.0f){
+            mass = 0.0f;
+        }
     }
 
-    void buttonClicked(){
-        // set display to 0
-        current_mass.text = "Mass: 0.000 g";
+    public void buttonClicked(){
+        if(unit_in_grams){
+            unit_in_grams = false;
+        }else{
+            unit_in_grams = true;
+        }
+    }
+
+    void OnMouseDown(){
+        if(unit_in_grams){
+            mass += 1.0f;
+        }else{
+            mass += 0.001f;
+        }
+    }
+
+    public void buttonDoneClicked(){
+        // give grade
+        grade.SetActive(true);
+        game.SetActive(false);
+        finishedScale();
+    }
+
+    private IEnumerator Wait()
+    {
+        // Wait for 1 second
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Laboratory Scene");
+    }
+
+    public void finishedScale(){
+        if(mass == goal){
+            scaleGrade = "A";
+        } else if((goal-mass) >= (goal-(.1*goal)) || (goal-mass) <= (goal+(.1*goal))){
+            scaleGrade = "B";
+        } else if ((goal-mass) >= (goal-(.2*goal)) || (goal-mass) <= (goal+(.2*goal))){
+            scaleGrade = "C";
+        } else {
+            scaleGrade = "F";
+        }
+        gradeUI.text = "Grade: " + scaleGrade;
+        //yield return new WaitForSeconds(2);
+        if(labStep == 3){
+            Lab.labStep = 4;
+            StartCoroutine(Wait());
+        }
+
     }
 }
